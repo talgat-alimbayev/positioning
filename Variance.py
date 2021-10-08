1db42ae69a6107f67baea6a2e5ba6267575277e1
talgat-alimbayev <alimbayev.talgat@gmail.com>
2021-10-08 16:41:03 +0600

    finished variance calculation

--- Variance.py
+++ Variance.py
@@ -1,10 +1,32 @@
 import os
+import json
 
 def variance(dir):
     list = os.listdir(dir)
     N = len(list) #number of samples to calculate variance
-    for i in range(3):
+    sum = 0.0
+    for i in range(N):
         os.path.join(dir,list[i])
         f = open( os.path.join(dir, list[i]), "r")
-        print(f.read())
-    print(list[0])
+        d = f.read()
+        data = json.loads(d)
+        x = data['beacons']['ab8190d5d11e4941acc4ac233f5b8a0427114cb9']
+        sum = sum + x
+    mean = sum/N
+
+    #calculating variance for a finite sample of measurements
+    variance = 0
+    for i in range(N):
+        os.path.join(dir,list[i])
+        f = open( os.path.join(dir, list[i]), "r")
+        d = f.read()
+        data = json.loads(d)
+        x = data['beacons']['ab8190d5d11e4941acc4ac233f5b8a0427114cb9']
+        variance = variance + ( x-mean )**2
+    variance = variance / (N-1)
+    print("mean: ", mean)
+    print("variance: ", variance)
+
+if __name__ == '__main__':
+    #Just put r before your normal string it converts normal string to raw string:
+    variance(r'C:\Users\tyalimbayev\OneDrive - ТОО Кар-тел\Рабочий стол\Positioning\PositionLogs\1_centerOfTheRoom')
