346cd74a3607f9ae8f6101d91967ff4f58747c43
Talgat Alimbayev <60476903+talgat-alimbayev@users.noreply.github.com>
2021-11-01 12:11:38 +0600

    Final version of rest api

--- kalman_rest_api.py
+++ kalman_rest_api.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from Tracker_KUIS import Tracker_KUIS
 
 app = Flask(__name__)
@@ -7,29 +7,21 @@ trackers = {}
 
 @app.route('/', methods=['POST'])
 def process_coordinates():
-    print("Hello world!")
-    # data = request.get_json()
-    print(request.json.get("protocol_version"))
-    return 1
+    data = request.get_json(force=True)
 
-    # data = json.loads(request.get_json())
-    #
-    # name = data["device_imei"]
-    # latitude = data["msg"]["sub_packet_1_data"]["values"]["latitude"]
-    # longitude = data["msg"]["sub_packet_1_data"]["values"]["longitude"]
-    # if name in trackers:
-    #     trackers[name].setMeas(latitude, longitude)
-    #     trackers[name].kalmanFilter()
-    # else:
-    #     trackers[name] = Tracker_KUIS(name, latitude, longitude)
-    #
-    # # changing latitude and longitude for filtered values
-    # data["msg"]["sub_packet_1_data"]["values"]["latitude"] = trackers[name].getLat()
-    # data["msg"]["sub_packet_1_data"]["values"]["longitude"] = trackers[name].getLong()
-    #
-    #
-    # return jsonify(data)
-try:
-    app.run(debug=True)
-except Exception as e:
-    print("An exception occurred: %s", e)
+    name = data["device_imei"]
+    latitude = data["msg"]["sub_packet_1_data"]["values"]["latitude"]
+    longitude = data["msg"]["sub_packet_1_data"]["values"]["longitude"]
+
+    if name in trackers:
+        trackers[name].setMeas(latitude, longitude)
+        trackers[name].kalmanFilter()
+    else:
+        trackers[name] = Tracker_KUIS(name, latitude, longitude)
+
+    # changing latitude and longitude for filtered values
+    data["msg"]["sub_packet_1_data"]["values"]["latitude"] = trackers[name].getLat()
+    data["msg"]["sub_packet_1_data"]["values"]["longitude"] = trackers[name].getLong()
+
+    return data
+app.run(debug=True)
